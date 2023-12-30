7fc4beff81e78f7fcada26d2eca84972cb598c8a
thabach <cbach@computer.org>
2023-12-30 12:42:29 +0100

    add validation dependency from jakarta

--- src/main/java/io/doubledispatch/bootcops/user/User.java
+++ src/main/java/io/doubledispatch/bootcops/user/User.java
@@ -2,21 +2,38 @@ package io.doubledispatch.bootcops.user;
 
 import java.util.Set;
 import java.util.UUID;
+
+import jakarta.persistence.ElementCollection;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotNull;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+
 
 @Getter
+@AllArgsConstructor
+// needed for JPA
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+@Table(name = "copsboot_user")
 public class User {
 
+    @Id
     private UUID id;
+
     private String email;
     private String password;
-    private Set<UserRole> roles;
 
-    public User(UUID id, String email, String password, Set<UserRole> roles) {
-        this.id = id;
-        this.email = email;
-        this.password = password;
-        this.roles = roles;
-    }
+    @ElementCollection(fetch = FetchType.EAGER)
+    @Enumerated(EnumType.STRING)
+    @NotNull
+    private Set<UserRole> roles;
 
 }
